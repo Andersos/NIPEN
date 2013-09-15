0ec126706edd8f5a7899928eb3ccb74bc72aa600
Anders Sandvik <anders@andersos.net>
2013-09-15 23:15:27 +0200

    Delete R.java

--- android-heart-rate/gen/com/jwetherell/heart_rate_monitor/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.jwetherell.heart_rate_monitor;
-
-/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
-public final class R {
-}
