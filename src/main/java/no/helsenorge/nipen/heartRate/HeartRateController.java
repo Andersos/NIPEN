7de62421258d90ec96ee7ba4eadd8868ab7d3486
Sebastian Zalewski <sebzalewski@gmail.com>
2013-09-16 12:01:38 +0200

    Trying to fix API

--- src/main/java/no/helsenorge/nipen/heartRate/HeartRateController.java
+++ src/main/java/no/helsenorge/nipen/heartRate/HeartRateController.java
@@ -2,6 +2,7 @@ package no.helsenorge.nipen.heartRate;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.List;
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-
+@Controller
 public class HeartRateController {
 
     @Autowired
