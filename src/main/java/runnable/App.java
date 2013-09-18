01dcc8d64f8885d18ce28efb914dfdbe850658ad
Sebastian Zalewski <sebzalewski@gmail.com>
2013-09-18 21:35:51 +0200

    Added App.java back

---
+++ src/main/java/runnable/App.java
@@ -0,0 +1,14 @@
+package runnable;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: 3820TG
+ * Date: 18.09.13
+ * Time: 17:57
+ * To change this template use File | Settings | File Templates.
+ */
+public class App {
+    public static void main(String[] args) {
+
+    }
+}
