56b947c3998e410431e4631ab1c1acba07707f81
lemrey <lemrey@gmail.com>
2013-09-23 12:49:50 +0200

    Trying to fix Travis

--- nipen-heart-rate/gen/no/helsenorge/nipen/android/HeartRate/R.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package no.helsenorge.nipen.android.HeartRate;
-
-/* This stub is for using by IDE only. It is NOT the R class actually packed into APK */
-public final class R {
-}
